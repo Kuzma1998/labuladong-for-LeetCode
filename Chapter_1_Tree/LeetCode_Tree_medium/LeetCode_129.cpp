0d5a601b0042dc38b4b83542f8755705036126da
Li Jiaxin <lijiaxin@csu.edu.cn>
2021-08-11 23:57:46 +0800

    2021-8-21

--- Chapter_1_Tree/LeetCode_Tree_medium/LeetCode_129.cpp
+++ Chapter_1_Tree/LeetCode_Tree_medium/LeetCode_129.cpp
@@ -39,3 +39,52 @@ public:
         s.pop_back(); 
     }
 };
+
+// 时间复杂度：O(n)其中 n 是二叉树的节点个数。对每个节点访问一次。
+
+// 空间复杂度：O(n)，其中 n 是二叉树的节点个数。空间复杂度主要取决于递归调用的栈空间，递归栈的深度等于二叉树的高度，
+// 最坏情况下，二叉树的高度等于节点个数，空间复杂度为 O(n)O(n)。
+
+
+// 使用广度优先搜索，需要维护两个队列，分别存储节点和节点对应的数字。
+// 初始时，将根节点和根节点的值分别加入两个队列。每次从两个队列分别取出一个节点和一个数字，进行如下操作：
+// 如果当前节点是叶子节点，则将该节点对应的数字加到数字之和；
+// 如果当前节点不是叶子节点，则获得当前节点的非空子节点，并根据当前节点对应的数字和子节点的值计算子节点对应的数字，
+// 然后将子节点和子节点对应的数字分别加入两个队列。
+// 搜索结束后，即可得到所有叶子节点对应的数字之和
+
+
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int sum = 0;
+        queue<TreeNode*> nodeQueue;
+        queue<int> numQueue;
+        nodeQueue.push(root);
+        numQueue.push(root->val);
+        while (!nodeQueue.empty()) {
+            TreeNode* node = nodeQueue.front();
+            int num = numQueue.front();
+            nodeQueue.pop();
+            numQueue.pop();
+            TreeNode* left = node->left;
+            TreeNode* right = node->right;
+            if (left == nullptr && right == nullptr) {
+                sum += num;
+            } else {
+                if (left != nullptr) {
+                    nodeQueue.push(left);
+                    numQueue.push(num * 10 + left->val);
+                }
+                if (right != nullptr) {
+                    nodeQueue.push(right);
+                    numQueue.push(num * 10 + right->val);
+                }
+            }
+        }
+        return sum;
+    }
+};
