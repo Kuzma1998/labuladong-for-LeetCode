5831f41c9d61bdf3ae0e534b6bdf306f77fa7a83
Li Jiaxin <1750108429@qq.com>
2021-12-07 14:44:16 +0800

    2021-12-7

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
  * @Author: Li Jiaxin
  * @Date: 2021-10-30 19:47:56
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-12-03 21:56:11
+ * @LastEditTime: 2021-12-07 09:19:37
  */
 #include <iostream>
 #include <string>
@@ -13,6 +13,7 @@
 #include <sstream>
 #include <stack>
 #include <deque>
+#include <queue>
 using namespace std;
 
 // class Solution {
@@ -323,63 +324,98 @@ using namespace std;
 //      cout << solver.evalRPN({"10","6","9","3","+","-11","*","/","*","17","+","5","+"}) << endl;
 //      return 0;
 // }
-class MyQueue
-{ //单调队列（从大到小）
+// class MyQueue
+// { //单调队列（从大到小）
+// public:
+//     deque<int> que; // 使用deque来实现单调队列
+//     // 每次弹出的时候，比较当前要弹出的数值是否等于队列出口元素的数值，如果相等则弹出。
+//     // 同时pop之前判断队列当前是否为空。
+//     void pop(int value)
+//     {
+//         if (!que.empty() && value == que.front())
+//         {
+//             que.pop_front();
+//         }
+//     }
+//     // 如果push的数值大于入口元素的数值，那么就将队列后端的数值弹出，直到push的数值小于等于队列入口元素的数值为止。
+//     // 这样就保持了队列里的数值是单调从大到小的了。
+//     void push(int value)
+//     {
+//         while (!que.empty() && value > que.back())
+//         {
+//             que.pop_back();
+//         }
+//         que.push_back(value);
+//     }
+//     // 查询当前队列里的最大值 直接返回队列前端也就是front就可以了。
+//     int front()
+//     {
+//         return que.front();
+//     }
+// };
+// class Solution
+// {
+// public:
+//     vector<int> maxSlidingWindow(vector<int> nums, int k)
+//     {
+//         MyQueue que;
+//         vector<int> result;
+//         for (int i = 0; i < k; i++)
+//         { // 先将前k的元素放进队列
+//             que.push(nums[i]);
+//         }
+//         result.push_back(que.front()); // result 记录前k的元素的最大值
+//         for (int i = k; i < nums.size(); i++)
+//         {
+//             que.pop(nums[i - k]);          // 滑动窗口移除最前面元素
+//             que.push(nums[i]);             // 滑动窗口前加入最后面的元素
+//             result.push_back(que.front()); // 记录对应的最大值
+//         }
+//         return result;
+//     }
+// };
+
+// int main()
+// {
+//     Solution solver;
+//     vector<int> vec;
+//     vec = solver.maxSlidingWindow({1, 3, -1, -3, 5, 3, 6, 7}, 3);
+//     for (auto i : vec)
+//         cout << i << endl;
+//     return 0;
+// }
+
+
+class Solution {
 public:
-    deque<int> que; // 使用deque来实现单调队列
-    // 每次弹出的时候，比较当前要弹出的数值是否等于队列出口元素的数值，如果相等则弹出。
-    // 同时pop之前判断队列当前是否为空。
-    void pop(int value)
-    {
-        if (!que.empty() && value == que.front())
-        {
-            que.pop_front();
+    vector<int> topKFrequent(vector<int> nums, int k) {
+        unordered_map<int,int> map;
+        priority_queue<pair<int,int>> pq;
+        vector<int> ans;
+        for(auto i:nums){
+            if(map.count(i))
+                map[i]++;
+            else
+                map[i] =1;
         }
-    }
-    // 如果push的数值大于入口元素的数值，那么就将队列后端的数值弹出，直到push的数值小于等于队列入口元素的数值为止。
-    // 这样就保持了队列里的数值是单调从大到小的了。
-    void push(int value)
-    {
-        while (!que.empty() && value > que.back())
-        {
-            que.pop_back();
+        for(auto v:map){
+            pair<int,int> p(v.first,v.second);
+            pq.push(p);
         }
-        que.push_back(value);
-    }
-    // 查询当前队列里的最大值 直接返回队列前端也就是front就可以了。
-    int front()
-    {
-        return que.front();
-    }
-};
-class Solution
-{
-public:
-    vector<int> maxSlidingWindow(vector<int> nums, int k)
-    {
-        MyQueue que;
-        vector<int> result;
-        for (int i = 0; i < k; i++)
-        { // 先将前k的元素放进队列
-            que.push(nums[i]);
+        for(int i=0;i<k;++i){
+            ans.push_back(pq.top().first);
+            pq.pop();
         }
-        result.push_back(que.front()); // result 记录前k的元素的最大值
-        for (int i = k; i < nums.size(); i++)
-        {
-            que.pop(nums[i - k]);          // 滑动窗口移除最前面元素
-            que.push(nums[i]);             // 滑动窗口前加入最后面的元素
-            result.push_back(que.front()); // 记录对应的最大值
-        }
-        return result;
+        return ans;
+
     }
 };
 
-int main()
-{
-    Solution solver;
-    vector<int> vec;
-    vec = solver.maxSlidingWindow({1, 3, -1, -3, 5, 3, 6, 7}, 3);
-    for (auto i : vec)
-        cout << i << endl;
-    return 0;
+
+int main(){
+      Solution solver;
+      vector<int> ans;
+      ans = solver.topKFrequent({-1,-1},1);
+      for(auto i:ans)
+        cout<<i<<endl;
 }
--- 抽象数据结构/栈和队列/347. 前 K 个高频元素.cpp
+++ 抽象数据结构/栈和队列/347. 前 K 个高频元素.cpp
@@ -2,5 +2,31 @@
  * @Description: 
  * @code: 
  * @Author: Li Jiaxin
- * @Date: 2021-12-04 11:38:25
+ * @Date: 2021-12-07 09:07:35
  */
+// 思路：利用map存频率，利用优先队列进行排序
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int> nums, int k) {
+        unordered_map<int,int> map;
+        priority_queue<pair<int,int>> pq;
+        vector<int> ans;
+        for(auto i:nums){
+            if(map.count(i))
+                map[i]++;
+            else
+                map[i] =1;
+        }
+        for(auto v:map){
+            pair<int,int> p(v.second,v.first);
+            pq.push(p);
+        }
+        for(int i=0;i<k;++i){
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+
+    }
+};
