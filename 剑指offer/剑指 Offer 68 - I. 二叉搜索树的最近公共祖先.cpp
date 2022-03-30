7529c382b8eefb1cf77f2b9cc0b986ad454b370a
Kuzma <1750108429@qq.com>
2022-03-30 16:22:24 +0800

    2022-3-29 offer twice

--- 剑指offer/剑指 Offer 68 - I. 二叉搜索树的最近公共祖先.cpp
+++ 剑指offer/剑指 Offer 68 - I. 二叉搜索树的最近公共祖先.cpp
@@ -20,3 +20,19 @@ public:
         return root;
     }
 };
+
+// bst 的性质 左小根小于右
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root)
+            return root;
+        if((p->val>root->val&&q->val<root->val)||(p->val<root->val&&q->val>root->val))
+            return root;
+        if(root->val==q->val||root->val==p->val)
+            return root;
+        if(q->val>root->val&&p->val>root->val)
+            return lowestCommonAncestor(root->right,p,q);
+        return lowestCommonAncestor(root->left,p,q);
+    }
+};
